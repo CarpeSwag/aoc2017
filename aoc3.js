5bc6d8ff4a0c3420a0485ada2f954aa355bf8431
CarpeSwag <james@garbagnati.com>
2017-12-03 12:48:47 -0800

    Finished problem 1 of day 3.

---
+++ aoc3.js
@@ -0,0 +1,37 @@
+let day3 = 265149;
+
+let p5 = {
+	problem: day3,
+	
+	tests: [
+		{problem: 1, solution: 0},
+		{problem: 12, solution: 3},
+		{problem: 23, solution: 2},
+		{problem: 10, solution: 3},
+		{problem: 9, solution: 2},
+		{problem: 25, solution: 4},
+		{problem: 28, solution: 3},
+		{problem: 27, solution: 4},
+		{problem: 29, solution: 4},
+		{problem: 26, solution: 5},
+		{problem: 30, solution: 5},
+		{problem: 49, solution: 6},
+		{problem: 31, solution: 6},
+		{problem: 1024, solution: 31}
+	],
+
+	solveFor: function(index) {
+		if (index == 1) return 0;
+		let sideLen = this.findSideLen(index);
+		let radius = Math.floor(sideLen/2);
+		let sidePos = (index - 1) % (sideLen - 1);
+		sidePos = (sidePos > radius)? (sideLen - 1) - sidePos: sidePos;
+		return radius + (radius - sidePos);
+	},
+	
+	findSideLen: function(index) {
+		let len = Math.ceil(Math.sqrt(index));
+		if (len % 2 == 0) ++len;
+		return len;
+	}
+};
