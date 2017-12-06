593e4307dbb6a6444f8107e6641cfe7e90e61b82
CarpeSwag <james@garbagnati.com>
2017-12-06 00:22:01 -0800

    Solved part 2 of day 6.

--- aoc6.js
+++ aoc6.js
@@ -45,3 +45,49 @@ let p11 = {
 		return output;
 	}
 };
+
+let p12 = {
+	problem: day6,
+	
+	tests: [
+		{problem: "0 2 7 0", solution: 4},
+		{problem: "1 0 0 0", solution: 4},
+		{problem: "7 0 0 0", solution: 4}
+	],
+
+	solveFor: function(blocks) {
+		let steps = 0;
+		
+		let lastState = blocks;
+		let states = {};
+		while (states[lastState] == null) {
+			states[lastState] = steps;
+			++steps;
+			lastState = this.redistribute(lastState.split(" "));
+		}
+		
+		return steps - states[lastState];
+	},
+	
+	redistribute: function(blocks) {
+		let largest = 0;
+		for (let i = 0; i < blocks.length; ++i)
+			if (+(blocks[i]) > +(blocks[largest]))
+				largest = i;
+		
+		let counter = blocks[largest]
+		blocks[largest] = 0;
+		let curr = (++largest) % blocks.length;
+		for (let i = counter; i > 0; --i) {
+			++blocks[curr];
+			curr = (++curr) % blocks.length;
+		}
+		
+		let output = blocks[0];
+		for (let i = 1; i < blocks.length; ++i) {
+			output += " " + blocks[i];
+		}
+		
+		return output;
+	}
+};
